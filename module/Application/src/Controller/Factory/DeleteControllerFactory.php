6da513abb0f3c67b2557f1d7459a51ad391b9b79
amykhailenko <amykhailenko@kv-po-amykhailenkonb.corp.vodafone.ua>
2017-07-18 10:11:08 +0300

    Change folder Model

--- module/Application/src/Controller/Factory/DeleteControllerFactory.php
+++ module/Application/src/Controller/Factory/DeleteControllerFactory.php
@@ -1,8 +1,5 @@
 <?php
+namespace Application\Controller\Factory;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+use Application\Controller\DeleteController;
+use Application\Model\DeviceCommandInterface;
