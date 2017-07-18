71d177f87f31927c5732fb4490cdb80ce35dc260
amykhailenko <amykhailenko@kv-po-amykhailenkonb.corp.vodafone.ua>
2017-07-18 16:49:18 +0300

    add package Form

---
+++ module/Application/src/Form/DeviceFieldset.php
@@ -0,0 +1,76 @@
+<?php
+namespace Application\Form;
+
+use Zend\Form\Fieldset;
+use Application\Model\Device;
+use Zend\Hydrator\Reflection as ReflectionHydrator;
+
+class DeviceFieldset extends Fieldset
+{
+    public function init() {
+        $this->setHydrator(new ReflectionHydrator());
+		$this->setObject(new Device('', '', '','', '', '',''));
+		
+        $this->add([
+            'type' => 'hidden',
+            'name' => 'id',
+        ]);
+
+        $this->add([
+            'type' => 'text',
+            'name' => 'model',
+            'options' => [
+                'label' => 'Device Title',
+            ],
+        ]);
+		
+	$this->add([
+            'type' => 'text',
+            'name' => 'category',
+            'options' => [
+                'label' => 'Device Category',
+            ],
+        ]);
+        
+        $this->add([
+            'type' => 'text',
+            'name' => 'manufacturer',
+            'options' => [
+                'label' => 'Device manufacturer',
+            ],
+        ]);
+        
+        $this->add([
+            'type' => 'text',
+            'name' => 'power',
+            'options' => [
+                'label' => 'Power',
+            ],
+        ]);
+        
+        $this->add([
+            'type' => 'text',
+            'name' => 'release_date',
+            'options' => [
+                'label' => 'Date of release',
+            ],
+        ]);
+        
+        $this->add([
+            'type' => 'text',
+            'name' => 'property',
+            'options' => [
+                'label' => 'Property',
+            ],
+        ]);
+
+        $this->add([
+            'type' => 'textarea',
+            'name' => 'description',
+            'options' => [
+                'label' => 'Description',
+            ],
+        ]);
+    }
+}
+
---
+++ module/Application/src/Form/DeviceForm.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application\Form;
+
+use Zend\Form\Form;
+
+class DeviceForm extends Form
+{
+    public function init() {
+        $this->add([
+            'name' => 'device',
+            'type' => DeviceFieldset::class,
+            'options' => [
+                'use_as_base_fieldset' => TRUE,
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Insert new device',
+            ],
+        ]);
+    }
+}
